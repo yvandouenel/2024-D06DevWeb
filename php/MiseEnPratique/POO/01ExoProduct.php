6b0c9b1e9c7f7963d26734628f2be291c375753a
yvandouenel <y.douenel@coopernet.fr>
2024-11-26 17:10:26 +0100

    Correction exo 2.1 Product et order.

--- php/MiseEnPratique/POO/01ExoProduct.php
+++ php/MiseEnPratique/POO/01ExoProduct.php
@@ -64,7 +64,7 @@ class Order
 {
   // Attributs d'instance privés (encapsulation)
   private array $pdtsQuantities;
-  
+
   // Attribut de class
   private static float $tva = 0.2;
 
@@ -72,22 +72,37 @@ class Order
   {
     $this->pdtsQuantities = $pdtsQuantities;
   }
-  
+  /**
+   * Ajoute des produits et leur quantité à la commande en cours
+   * 
+   * @return Order
+   */
+  public function addProducts($product, $quantity)
+  {
+    // Ajout du tableau associatif produc et quantity au tableau indexé $this->pdtsQuantities
+    $this->pdtsQuantities[] = ["product" => $product, "quantity" => $quantity];
+
+    // On répercute sur le stock :
+    $product->removeStock($quantity);
+
+    return $this;
+  }
   public function calculateTotal(): float
   {
     $result = 0;
 
     // Parcours le tableau $pdtsQuantities et multiplie le prix de chaque produit par la quantité
     // Renvoie la somme toutes les multiplication en incluant la tva
-    foreach($this->pdtsQuantities as  $pdtQuantities){
-      pdtQuantities["product"]
+    foreach ($this->pdtsQuantities as  $pdtQuantities) {
+      $result += $pdtQuantities["product"]->getPrice() * $pdtQuantities["quantity"] * (1 + Order::$tva);
     }
-
+    return $result;
   }
-  
 }
-// Instanciation
-$order1 = new Order([
-  ["product"=>$computer, 10],
-  ["product"=>$screen, 10],
-]);
+// Instanciation avec pour argument un tableau de tableaux associatifs
+$order1 = new Order();
+$order1->addProducts($computer, 1);
+// Afficher le total
+echo "Total pour la commande : " . $order1->calculateTotal() . "€TTC";
+
+var_dump($computer);
