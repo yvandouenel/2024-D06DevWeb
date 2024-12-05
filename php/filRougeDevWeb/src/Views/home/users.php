7c8352ce0689ad51929dd59169105a3156c399a4
yvandouenel <y.douenel@coopernet.fr>
2024-12-05 14:13:19 +0100

    Ajout de styles sass.

--- php/filRougeDevWeb/src/Views/home/users.php
+++ php/filRougeDevWeb/src/Views/home/users.php
@@ -1,11 +1,11 @@
-<div>
+<div class="container">
     <h1>Utilisateurs</h1>
     <?php //var_dump($users); 
     ?>
-    <section class="users-list">
+    <section class="users-list altern-grey">
         <?php if (!empty($users)): ?>
             <?php foreach ($users as $user): ?>
-                <section class="d-flex gap-2">
+                <section class="d-flex gap-2 border-1">
                     <p><?= htmlspecialchars($user['id']) ?></p>
                     <p><?= htmlspecialchars($user['name']) ?></p>
                     <p><?= htmlspecialchars($user['email']) ?></p>
