4034ff32f0db2d92a5460ac00d20fe697a7a1a8b
yvandouenel <y.douenel@coopernet.fr>
2024-12-16 15:05:36 +0100

    Fin exo 3

--- TypeScript/03testNarrowing.js
+++ TypeScript/03testNarrowing.js
@@ -5,5 +5,16 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("input");
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
     console.log(`Boutton cliqué`);
-    input.value = "100";
+    // Narrowing
+    if (input)
+        input.value = "100";
 });
+class Test {
+    constructor(tasks) {
+        this.tasks = tasks;
+    }
+}
+const test = new Test([
+    { id: "qsdf", title: "Titre de la tâche", done: true },
+    { id: "fgururuy", title: "Titre de la tâche 2", done: true },
+]);
--- TypeScript/03testNarrowing.ts
+++ TypeScript/03testNarrowing.ts
@@ -2,9 +2,28 @@
 const btn: HTMLElement | null = document.getElementById("btn");
 
 // Je fais du rétrécissement pour indiquer plus précisément le type de l'élément du DOM (ici HTMLInputElement )
-const input = document.getElementById("input") as HTMLInputElement;
+const input = document.getElementById("input") as HTMLInputElement | null;
 
 btn?.addEventListener("click", () => {
   console.log(`Boutton cliqué`);
-  input.value = "100";
+  // Narrowing
+  if (input) input.value = "100";
 });
+
+// Création d'une interface TaskInterface
+interface TaskInterface {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
+class Test {
+  tasks!: TaskInterface[];
+  constructor(tasks: TaskInterface[]) {
+    this.tasks = tasks;
+  }
+}
+const test = new Test([
+  { id: "qsdf", title: "Titre de la tâche", done: true },
+  { id: "fgururuy", title: "Titre de la tâche 2", done: true },
+]);
