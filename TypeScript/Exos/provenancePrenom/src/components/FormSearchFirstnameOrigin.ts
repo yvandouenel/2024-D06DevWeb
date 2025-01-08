9f931b7930ddc0b45ab7f6d851954e73febd00ba
yvandouenel <y.douenel@coopernet.fr>
2025-01-08 16:37:04 +0100

    Ajout des catch sur les services.

--- TypeScript/Exos/provenancePrenom/src/components/FormSearchFirstnameOrigin.ts
+++ TypeScript/Exos/provenancePrenom/src/components/FormSearchFirstnameOrigin.ts
@@ -33,36 +33,50 @@ export default class FormSearchFirstnameOrigin {
       console.log(`this.domElts.input.value`, this.domElts.input.value);
       // Appel du service
       if (firstname) {
-        const firstnameOriginArray = await Fetch.loadFirstNameCountries(
-          firstname
-        );
-        console.log(`firstnameOrigin`, firstnameOriginArray);
+        try {
+          const firstnameOriginArray = await Fetch.loadFirstNameCountries(
+            firstname
+          );
+          console.log(`firstnameOrigin`, firstnameOriginArray);
 
-        let sectionCountries = document.getElementById("section-countries");
-        // Si la section existe déjà, on la supprime
-        if (sectionCountries) sectionCountries.remove();
+          let sectionCountries = document.getElementById("section-countries");
+          // Si la section existe déjà, on la supprime
+          if (sectionCountries) sectionCountries.remove();
 
-        // Création de la section qui affiche les pays
-        sectionCountries = createMarkup("section", document.body, "", {
-          id: "section-countries",
-          class: "container",
-        });
-
-        firstnameOriginArray.country.forEach((country: CountryCode) => {
-          console.log(`country_id`, country.country_id);
-          const { probability } = country;
-          // Appel du service pour traduire le code du pays
-
-          Fetch.loadCountriesCode(country.country_id).then((countries) => {
-            countries.forEach((country) => {
-              console.log(`country`, country);
-
-              sectionCountries.innerHTML += `<p><strong>${
-                country.name.common
-              }</strong> : ${(probability * 100).toFixed(2)} %</p>`;
-            });
+          // Création de la section qui affiche les pays
+          sectionCountries = createMarkup("section", document.body, "", {
+            id: "section-countries",
+            class: "container",
           });
-        });
+
+          firstnameOriginArray.country.forEach((country: CountryCode) => {
+            console.log(`country_id`, country.country_id);
+            const { probability } = country;
+            // Appel du service pour traduire le code du pays
+
+            Fetch.loadCountriesCode(country.country_id)
+              .then((countries) => {
+                countries.forEach((country) => {
+                  console.log(`country`, country);
+
+                  sectionCountries.innerHTML += `<p><strong>${
+                    country.name.common
+                  }</strong> : ${(probability * 100).toFixed(2)} %</p>`;
+                });
+              })
+              .catch((error) => {
+                console.error(
+                  `Erreur attrapée lors de l'appel de loadCountriesCode`,
+                  error
+                );
+              });
+          });
+        } catch (error) {
+          console.error(
+            "Erreur attrapée lors de l'appel de loadFirstNameCountries",
+            error
+          );
+        }
       }
     });
   }
